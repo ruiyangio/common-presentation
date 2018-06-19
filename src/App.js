5085324b8203c5c5ea533ca44a0f439afa323ee2
ruiyangio <ruiyangwind@gmail.com>
2018-06-19 15:57:05 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Reveal from 'reveal.js';
 import '../node_modules/reveal.js/css/reveal.css';
-import '../node_modules/reveal.js/css/theme/white.css';
+import '../node_modules/reveal.js/css/theme/moon.css';
 import './App.css';
 
 class App extends Component {
@@ -14,10 +14,11 @@ class App extends Component {
       <div className="demo reveal">
         <div className="slides">
           <section>
-            <header>S1</header>
+            <header className="main-header">S1 Title</header>
             <div className="slide-body">S1</div>
           </section>
           <section>S2</section>
+          <section>S3</section>
         </div>
       </div>
     );
