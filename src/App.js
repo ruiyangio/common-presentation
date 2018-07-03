2d8b291e1ff15e0613fc9413be0251ef210ca4e4
ruiyangio <ruiyangwind@gmail.com>
2018-07-03 11:54:45 -0700

    Add common libs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import Reveal from 'reveal.js';
+import MathJax from 'react-mathjax2';
 import '../node_modules/reveal.js/css/reveal.css';
 import '../node_modules/reveal.js/css/theme/moon.css';
 import './App.css';
 
+const tf = 'tf(t,d) = log(1 + f_(t,d))';
+
 class App extends Component {
   componentDidMount() {
-    Reveal.initialize();
+    Reveal.initialize({
+      width: '100%',
+      height: '100%'
+    });
   }
 
   render() {
@@ -15,7 +21,13 @@ class App extends Component {
         <div className="slides">
           <section>
             <header className="main-header">S1 Title</header>
-            <div className="slide-body">S1</div>
+            <div className="slide-body">
+              <MathJax.Context input="ascii">
+                <div>
+                  <MathJax.Node>{tf}</MathJax.Node>
+                </div>
+              </MathJax.Context>
+            </div>
           </section>
           <section>S2</section>
           <section>S3</section>
